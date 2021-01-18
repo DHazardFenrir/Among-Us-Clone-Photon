2de8fd242515b6b9df1b46e1c6acf2bd913ed0f8
DESKTOP-VPHORI4\kille <dzharzard96@gmail.com>
2021-01-17 20:52:10 -0600

    Se arregla el bug de sincronizacion e implementa movimiento independiente. Se prepara el personaje para poner su nick encima de su avatar.

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSetup : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!photonView.IsMine){
+            GetComponent<CharacterController>().enabled = false;
+        }
+    }
+
+    
+}
