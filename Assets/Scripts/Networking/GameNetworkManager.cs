4d5c615d4d1ddc71c185641f51f8f95a3833ea87
Gold <dzharzard96@gmail.com>
2021-02-16 16:11:32 -0600

    PhotonEvents listos.

--- Assets/Scripts/Networking/GameNetworkManager.cs
+++ Assets/Scripts/Networking/GameNetworkManager.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
 
 public class GameNetworkManager : MonoBehaviour
 {
     [SerializeField] GameObject playerPrefab = default;
     [SerializeField] Transform spawnPoint = default;
-
+   
     private void Start()
     {
         if (PhotonNetwork.IsConnected)
             PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, spawnPoint.rotation);
 
-     
-
+        if(PhotonNetwork.IsMasterClient)
+        StartCoroutine(StartGameDelay());
        
+    }
+
+    private IEnumerator StartGameDelay()
+    {
+        yield return new WaitForSeconds(2f);
+        //ejecutar evento en photon
+        object content = null;
+        RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All };
+        PhotonNetwork.RaiseEvent(NetworkingEventsCodes.StartGameEventCode, content, raiseEventOptions, SendOptions.SendReliable );
     }
 }
---
+++ Assets/Scripts/UI/InGame/StartGameView.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+
+public class StartGameView : MonoBehaviour, IOnEventCallback
+{
+    private CanvasGroup canvasGroup;
+
+    private void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    private void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    public void HideScreen()
+    {
+        canvasGroup.DOFade(0f, 1f);
+    }
+
+    public void OnEvent(EventData photonEvent)
+    {
+        byte eventCode = photonEvent.Code;
+
+        if(eventCode == NetworkingEventsCodes.StartGameEventCode)
+        {
+            object data = photonEvent.CustomData;
+            HideScreen();
+            
+        }
+        
+    }
+}
