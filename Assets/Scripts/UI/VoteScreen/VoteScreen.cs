546e76a681e4a77edc4635a195244b25de11ec83
Gold <dzharzard96@gmail.com>
2021-04-20 21:37:07 -0500

    Se termino de ver la clase 20. Empezando clase 21.

---
+++ Assets/Scripts/UI/VoteScreen/VoteScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+public class VoteScreen : MonoBehaviour
+{
+    [SerializeField] GameObject playerRow = default;
+    private void CreatePlayerRows()
+    {
+        Player[] players = PhotonNetwork.PlayerList;
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            CreateRow(players[i]);
+        }
+    }
+
+    private void CreateRow(Player player)
+    {
+        Hashtable hash = player.CustomProperties;
+
+    }
+}
